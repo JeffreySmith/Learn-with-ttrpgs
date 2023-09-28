3d015991298919029cccda7e9f881594848184be
Jeffrey Smith <jeffrey.smith7@gmail.com>
2023-09-28 13:24:32 -0400

    Really basic user login added, with appropriate http status codes

--- index.ts
+++ index.ts
@@ -6,54 +6,102 @@ const sqlite3 = require("sqlite3");
 const port = 8000;
 
 const my_session = {
-  secret:crypto.randomUUID(),
-  resave:false,
-  saveUninitialized:true
+  secret: crypto.randomUUID(),
+  resave: false,
+  saveUninitialized: true
 };
 
-const app:Express = express();
+declare module 'express-session'{
+  interface SessionData{
+    loggedIn:boolean;
+    username:string;
+  }
+}
+
+
+const app: Express = express();
 app.use(session(my_session));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-let db = new sqlite3.Database("./SQL/testDB.db", (err:any) => {
+app.use(express.urlencoded({ extended: true }));
+let db = new sqlite3.Database("./SQL/testDB.db", (err: any) => {
   if (err) {
     console.error(err.message);
   }
 });
 
-let user = {};
-db.serialize(()=>{
-    db.each("select * from Users",
-	    (err:any,row:any)=>{
-		if(err){
-		    console.error(err.message);
-		}
-		user = {id:row.ID,
-			name:row.Name,
-			email:row.Email
-		       };
-		let jSON = JSON.stringify(user);
-		console.log(jSON);
-		console.log(row.ID+"\t"+row.Name);
-	    });
+type User = {
+  name:string;
+  email:string;
+  password:string;
+  id:number
+};
+let users:User[] = [];
+
+db.serialize(() => {
+  db.each("select * from Users",
+    (err: any, row: any) => {
+      if (err) {
+        console.error(err.message);
+      }
+      let user:User = {
+        id: row.ID,
+        name: row.Name,
+        email: row.Email,
+        password: row.Password
+      };
+      
+      let jSON = JSON.stringify(user);
+      users.push(user);
+      console.log(jSON);
+      console.log(row.ID + "\t" + row.Name);
+    });
 });
 
-app.get("/",(req:Request,res:Response)=>{
+app.get("/", (_req: Request, res: Response) => {
   res.send("Hello there!");
 });
-app.get("/user",(req:Request,res:Response)=>{
-  res.json(user);
+app.get("/user", (_req: Request, res: Response) => {
+  let a = JSON.stringify(users[0]);
+  res.json(a);
 });
-app.post("/login",(req:Request,res:Response)=>{
-  console.log(req.body);
-  res.send(req.body);
-  if(req.body.username){
+app.post("/login", (req: Request, res: Response) => {
+  if (req.body.username) {
     console.log(`Username is: ${req.body.username}`);
   }
-  if(req.body.password){
+  if (req.body.password) {
     console.log(`Password is ${req.body.password}`);
   }
+  let args = 0;
+  for (let arg in req.body){
+    args+=1;
+  }
+  if(args > 2){
+    res.status(400).send("Incorrect format");
+  }
+  
+  let successfull = false;
+  let email = "";
+  let username = "";
+  for(let user of users){
+    if(req.body.username === user.email && req.body.password === user.password){
+      successfull = true;
+      email = user.email;
+      username = user.email;
+    }
+  }
+  if(successfull){
+    req.session.loggedIn = true;
+    req.session.username = email;
+    
+    res.status(200).send(`Succesfully logged in as ${username}`);
+
+  }
+  else{ 
+    res.status(401).send('Failed to find user');
+  }
 });
-app.listen(port,()=>{
+
+
+app.listen(port, () => {
   console.log(`Running server on port ${port}`);
 });
