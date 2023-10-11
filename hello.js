f99e362de43b9cb57f11fc48776a8a6194579fc1
Jeffrey Smith <jeffrey.smith7@gmail.com>
2023-10-11 10:11:24 -0400

    Actually added the js file this time

---
+++ hello.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+const index_1 = require("./index");
+function hello(name) {
+    (0, index_1.test)();
+}
+exports.hello = hello;
+hello("Jeffrey");
