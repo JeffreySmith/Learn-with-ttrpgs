965127bd54b5161bdf4ffaf514940b4cf12e44bb
Jeffrey Smith <jeffrey.smith7@gmail.com>
2023-10-11 10:11:44 -0400

    Removed unused file

--- hello.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
-const index_1 = require("./index");
-function hello(name) {
-    (0, index_1.test)();
-}
-exports.hello = hello;
-hello("Jeffrey");
