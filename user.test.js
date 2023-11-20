697923088033a66b4fb471135b7dc52de2d88ea1
Jeffrey Smith <jeffrey.smith7@gmail.com>
2023-11-20 17:37:23 -0500

    New unit test, verbose test running

--- user.test.js
+++ user.test.js
@@ -6,3 +6,7 @@ test("Check that the user search works",()=>{
   let user = User.findUserSafe(email);
   expect(user.email).toEqual(email);
 })
+test("User SQL injection test",()=>{
+  //This shouldn't return a valid result
+  expect(()=>User.getUserById("1 OR id=2")).toBeTruthy(undefined);
+});
