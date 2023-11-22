e5340bcdb4a73ffb1cc4f738c325ba0cd3dbfa26
Jeffrey Smith <jeffrey.smith7@gmail.com>
2023-11-21 21:31:48 -0500

    Added a few more unit tests

--- session.test.js
+++ session.test.js
@@ -10,3 +10,7 @@ test("Test grade calculation of testtranscript2.txt",()=>{
 test("Test SQL injection",()=>{
   expect(()=>session.findSession("2 or 1=1")).toThrow(TypeError);
 });
+test("Rounding function works correctly",()=>{
+  expect(session.round(1.26)).toBe(1.25);
+});
+
